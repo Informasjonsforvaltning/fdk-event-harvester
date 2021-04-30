f9937184328346fef2437c99b8bd9a9d90f172da
NilsOveTen <github.nils.ove@tendenes.com>
2021-04-30 10:41:30 +0200

    fix: rm fuseki-properties

--- src/main/java/no/fdk/fdk_event_harvester/Application.java
+++ src/main/java/no/fdk/fdk_event_harvester/Application.java
@@ -1,14 +1,13 @@
 package no.fdk.fdk_event_harvester;
 
 import no.fdk.fdk_event_harvester.configuration.ApplicationProperties;
-import no.fdk.fdk_event_harvester.configuration.FusekiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
-@EnableConfigurationProperties({ApplicationProperties.class, FusekiProperties.class})
+@EnableConfigurationProperties({ApplicationProperties.class})
 public class Application {
 
     public static void main(String[] args) {
